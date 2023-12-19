aec92db11e2bd68f652d1392844f1f242bfcee24
Abdul-Ganue <derichest01@gmail.com>
2023-12-19 03:36:20 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Code_Css_Html_Js
+ 
---
+++ index.js
@@ -0,0 +1,10 @@
+let imag=document.getElementById("imag");
+ let imageSource=["a.jpg","b.jpg","d.jpg","c.jpg"]
+
+let num=0;
+
+function img(){
+imag.innerHTML="fuck"
+}
+
+setInterval(img,1000);
